ff5c696d3db2807fde1c72fa77428e8b9e22d22f
Henry Koiti Arasaki <82346140+HenryArasaki@users.noreply.github.com>
2022-03-22 10:54:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,34 @@ document.addEventListener('keydown',(e)=>{
     notas.forEach(element => {
         if (e.key==element.dataset.key){
         element.play()
+        fazAnimacao(element)
         }
     });
 
 })
+
+const piano = document.getElementById("piano")
+
+let viewportHeight = window.innerHeight;
+let viewportWidth = window.innerWidth;
+
+notas.forEach(element=>{
+    const bola = document.createElement('div')
+    bola.classList.add('bola')
+    bola.style.left=(Math.floor((Math.random() * (viewportWidth-350))+100)+'px')
+    bola.style.top=(Math.floor((Math.random() * (viewportHeight-350))+100)+'px')
+    bola.dataset.key=element.dataset.key
+    piano.append(bola)
+})
+
+const bolas = document.getElementsByClassName('bola')
+function fazAnimacao(element){
+    for(let bola of bolas){
+        console.log(bola.dataset.key)
+        if(element.dataset.key==bola.dataset.key){
+            bola.classList.add('ativo')
+            setTimeout(()=>bola.classList.remove('ativo'),300)
+        }
+    }
+}
+
